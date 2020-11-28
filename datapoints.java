66605d8a8f147330de2cbb4bbbb425473740bdb7
ab7261 <60058666+ab7261@users.noreply.github.com>
2020-11-28 04:00:54 -0500

    Add files via upload
    Outline for file processing and datapoints.

---
+++ datapoints.java
@@ -0,0 +1,26 @@
+package NLP_FINAL;
+
+public class datapoints{ //object to help translate results for gmm
+ 
+//fillers for features, can add more/subtract as needed, double works best I think
+    int feature1;
+    int feature2;
+    int feature3;
+    double feature4;
+    double feature5;
+    double feature6;
+
+    public datapoints(int a, int b, int c, double d, double e, double f)
+    {
+feature1=a;
+feature2=b;
+feature3=c;
+feature4=d;
+feature5=e;
+feature6=f;
+    }
+
+
+    
+    
+}
---
+++ features.java
@@ -0,0 +1,67 @@
+package NLP_FINAL;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+//This program is gonna collect the data that I'm gonna put into the GMM, input is a directory that is called along with the file
+//
+//Program runs through directory and calculates features. Program assumes genres are going to be listed in line one as numbers, separated by spaces
+public class features {
+    static ArrayList[] genres = new ArrayList[11]; //support about 11 genres so far (we can tweak)
+    static ArrayList<Integer> genreHelper = new ArrayList<Integer>();
+    public static void main(String[] args) throws IOException// Call this with path to directory
+    {
+        File f = new File(args[0]);
+        if (!f.isDirectory()) // Input validation/debugging
+        {
+            System.out.println("Not a directory");
+            System.exit(0);
+        }
+        File[] accessDirectory = f.listFiles(); // array of files
+
+        for (int i = 1; i < accessDirectory.length; i++) // the i=1 is Mac-Specific (maybe linux too) (skips over the hidden .dsstore file that's in every directory and was causing a bunch of issues)
+        {
+            byte [] fileConverter = Files.readAllBytes(accessDirectory[i].toPath()); 
+            String fileText = new String(fileConverter,StandardCharsets.UTF_8); //Turns file into String
+            int genrelocation = fileText.indexOf("#$"); //two characters to put after genres so program knows where they're listed
+            String genres = fileText.substring(0, genrelocation); //kinda messy, but String.split isn't working for some reason so this is what we're going with
+            String [] listOfGenres = genres.split(" "); // 3 genres max, planning on using imdb as the gold standard for genres to compare output to
+            for(int s = 0; s<listOfGenres.length; s++)
+            {
+                genreHelper.add(Integer.parseInt(listOfGenres[s]));
+            }
+            //this part of the program's gonna do all of the heavy lifting, remember to use substring to skip over the #$ sequence that separates genre from actual script text
+            //I'll add regexes and other shit here to capture data for simpler features (ratio of adjectives to nouns)
+            //we can also call methods directly on the string for more complex features (tfidf) if easier
+            
+            
+            
+            //create datapoint object from data here (variable will be referred to as datum) 
+           
+            /*
+            for(int j = 0; j<genreHelper.size();j++)
+            {
+                genres[genreHelper.get(j)].add(datum)
+            }
+            */
+
+            
+
+          
+            
+
+
+        }
+
+
+
+
+        //program will convert arraylist to matrices here (or whatever format scikit uses)
+
+
+
+    }
+    
+}
